08733793e14001a893447f0240d7efaf0ab646d2
dzhiqiang <584462393@qq.com>
2016-11-19 22:42:50 +0800

    修改

--- src/main/java/dzq/group/mark/controller/UserController.java
+++ src/main/java/dzq/group/mark/controller/UserController.java
@@ -10,17 +10,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	
-	public static final Logger logger = Logger.getLogger(UserController.class);
-	
-	@RequestMapping(value="/login",produces="text/html;charset=UTF-8",method=RequestMethod.POST)
-	@ResponseBody
-	public String login(@RequestParam String mobilePhone
-			,@RequestParam String password){
-		logger.info("mobilePhone:"+mobilePhone+"login");
-		
-		System.out.println("121");
-		return "{name:123}";
-	}
-	
+
+    public static final Logger logger = Logger.getLogger(UserController.class);
+
+    @RequestMapping(value = "/login", produces = "text/html;charset=UTF-8", method = RequestMethod.POST)
+    @ResponseBody
+    public String login(@RequestParam String mobilePhone
+            , @RequestParam String password) {
+        logger.info("mobilePhone:" + mobilePhone + "login");
+
+        System.out.println("121");
+        return "{name:123}";
+    }
+
+    @RequestMapping(value = "/register", produces = "text/html;charset=UTF-8", method = RequestMethod.POST)
+    @ResponseBody
+    public String register(@RequestParam String nickName, @RequestParam String mobilePhone
+            , @RequestParam String password) {
+        return "";
+    }
+
+    @RequestMapping(value = "/register", produces = "text/html;charset=UTF-8", method = RequestMethod.POST)
+    @ResponseBody
+    public String searchUser(@RequestParam String mobilePhone) {
+        return "";
+    }
+
+
 }
